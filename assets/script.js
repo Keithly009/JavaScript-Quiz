15dcb82d33f6510ca1367b5b4888fd51daae4cc7
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-10-25 21:23:54 -0700

    Added more to JS in relation to the base Index.html folder

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,22 @@
 // Implement a score list at the end of the Quiz 
+var ScoreList = document.getElementById("ScoresList"); 
+var score = JSON.parse(localStorage.getItem("score")) || []; 
+ScoreList.innerHTML = score.map(score => { 
+    return `<li class="score">${score.name} - ${score.score}</li>`; 
+}).join('')
+
+var resetScore = document.querySelector(".reset-button"); 
 // Add function to reset Scores list 
+function resetScore() {
+    localStorage.clear(); 
+    window.location.reload("./index.html");
+}; 
 // Listener for Reset Button 
-// Variable of letters 
-// Add Drop / Draw function 
+resetScore.addEventListener("click", resetHighScores);
+
+// Variable of letters
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ!@#$%^&*' 
+letters = letters.split(''); 
+
+var fontSize=10, 
+    columns = canvas.width / fontSize; 
