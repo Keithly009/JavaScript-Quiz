72e19b6c49ebddf425ba98caa4f07871398596c8
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-10-25 21:01:39 -0700

    removed unnecessary code and beginning to implement score function & JS for the gamepage, Questions still not popping up

---
+++ assets/gp.js
@@ -0,0 +1,58 @@
+const question = document.getElementById("question"); 
+const choices = Array.from(document.getElementByClassName*('choice-text')); 
+
+
+// Add Questions 
+var quizdata = [ 
+    {
+        question: "Javascript is what kind of language? Choose the answer that best fits!",
+        a: "very interesting", 
+        b: "Easily learned", 
+        c: "Dynamically typed",
+        d: "statically typed", 
+        correct: "c",  
+        
+        question: "A series of characters that is written within quotations is a what?", 
+        a: "Boolean", 
+        b: "Number", 
+        c: "Swallow", 
+        d: "String", 
+        correct: "d",
+
+        question: "As a non-primitive data type, I can do what?", 
+        a: "Write only a single value in the data type", 
+        b: "Can only store two values in the data type", 
+        c: "Can store as many values as needed, but they cannot be complex values", 
+        d: "Can store multiple and complex value types in this data type", 
+        correct: "d", 
+
+        question: "The act of moving all declared funtion and variables to the top is known as what?", 
+        a: "Boolean", 
+        b: "Hoisting", 
+        c: "Elevate", 
+        d: "Shifting",
+        correct: "b", 
+        
+        question: "Which of the following operators will return the value as true? Assume that both variables are the same number and are not strings.", 
+        a: "x == y", 
+        b: "x===y", 
+        c: "x ~ y",
+        d: "y + x",  
+        correct: "a", 
+        
+        question: "When combining a number to a string or two strings together it is known as a what?", 
+        a: "String", 
+        b: "string coercion", 
+        c: "Number string", 
+        d: "double string", 
+        correct: "b", 
+
+        question: "A true or false value is known as a what?", 
+        a: "Boolean", 
+        b: "Number", 
+        c: "String", 
+        d: "Hoisting", 
+        correct: "a",
+    },
+
+];
